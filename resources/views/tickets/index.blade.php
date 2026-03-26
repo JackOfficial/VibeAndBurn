23669d7631352f0cad360682780a2797e48cb1d4
JackOfficial <musengimanajacques@gmail.com>
2026-03-26 10:01:58 +0200

    Refining Vibe&Burn

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -1,30 +1,25 @@
 @extends('user dashboard.dashboard')
-@section('title', 'Vibe&burn - View Ticket #' . $ticketId)
+@section('title', 'Vibe&burn - My Tickets')
 
 @section('content')
 <div class="nk-content-inner">
     <div class="nk-content-body">
         <div class="components-preview wide-md mx-auto">
             <div class="nk-block-head nk-block-head-sm">
-                <div class="nk-block-between">
-                    <div class="nk-block-head-content">
-                        <h3 class="nk-block-title page-title">Ticket Support</h3>
-                        <div class="nk-block-des text-soft">
-                            <p>View conversation for Ticket #{{ $ticketId }}</p>
-                        </div>
-                    </div>
-                    <div class="nk-block-head-content">
-                        <div class="toggle-wrap nk-block-tools-toggle">
-                            {{-- Change '#' to your actual route name --}}
-                            <a href="{{ route('user.tickets.list') }}" class="btn btn-outline-light btn-white">
-                                <em class="icon ni ni-arrow-left"></em><span>Back to List</span>
-                            </a>
-                        </div>
+                <div class="nk-block-head-content">
+                    <h3 class="nk-block-title page-title">Support Tickets</h3>
+                    <div class="nk-block-des text-soft">
+                        {{-- Use the $wallet variable passed from your controller --}}
+                        <p>Manage your support requests. Current Balance: {{ number_format($wallet, 2) }} RWF</p>
                     </div>
                 </div>
-            </div><div class="nk-block">
-                {{-- Passing the ID to your Livewire Component --}}
-                @livewire('user.ticket-detail-component', ['id' => $ticketId])
-            </div></div></div>
+            </div>
+
+            <div class="nk-block">
+                {{-- This component lists all tickets; it doesn't need an ID passed to it --}}
+                @livewire('user.user-ticket-component')
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
