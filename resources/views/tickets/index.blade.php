0fb075e5a7d22662e584d7795c5bb7b47896a4d0
JackOfficial <musengimanajacques@gmail.com>
2026-03-26 10:05:05 +0200

    Refining Vibe&Burn

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -5,15 +5,6 @@
 <div class="nk-content-inner">
     <div class="nk-content-body">
         <div class="components-preview wide-md mx-auto">
-            <div class="nk-block-head nk-block-head-sm">
-                <div class="nk-block-head-content">
-                    <h3 class="nk-block-title page-title">Support Tickets</h3>
-                    <div class="nk-block-des text-soft">
-                        {{-- Use the $wallet variable passed from your controller --}}
-                        <p>Manage your support requests. Current Balance: {{ number_format($wallet, 2) }} RWF</p>
-                    </div>
-                </div>
-            </div>
 
             <div class="nk-block">
                 {{-- This component lists all tickets; it doesn't need an ID passed to it --}}
