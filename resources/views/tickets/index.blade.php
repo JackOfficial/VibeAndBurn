e900493e12acac6eb6628b778512e9be5da239bc
JackOfficial <musengimanajacques@gmail.com>
2026-03-26 11:10:36 +0200

    Refining Vibe&Burn

---
+++ resources/views/tickets/index.blade.php
@@ -0,0 +1,16 @@
+@extends('user dashboard.dashboard')
+@section('title', 'Vibe&burn - My Tickets')
+
+@section('content')
+<div class="nk-content-inner">
+    <div class="nk-content-body">
+        <div class="components-preview wide-md mx-auto">
+
+            <div class="nk-block">
+                {{-- This component lists all tickets; it doesn't need an ID passed to it --}}
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
