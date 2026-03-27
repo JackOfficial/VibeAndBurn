f1edf06d340e53fec5c3c3aea7e6475e0f460beb
JackOfficial <musengimanajacques@gmail.com>
2026-03-27 16:41:43 +0200

    fixing

--- app/Http/Controllers/admin/RateController.php
+++ app/Http/Controllers/admin/RateController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class RateController extends Controller
 {
     public function index(){
-        return view('admin.manage.rate');
+        return view('admin.rates.index');
     }
 }
