9a3e0e37d3a5a3e6efa9ea386e1d759056826385
JackOfficial <musengimanajacques@gmail.com>
2026-03-27 16:29:47 +0200

    fixing

--- resources/views/admin/funds/index.blade.php
+++ resources/views/admin/funds/index.blade.php
@@ -10,7 +10,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">Home</a></li>
+              <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">Home</a></li>
               <li class="breadcrumb-item active">Funds</li>
             </ol>
           </div>
