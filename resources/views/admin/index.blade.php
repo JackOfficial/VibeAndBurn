a3170af8d180934603ab9d7f0832ca008822f304
JackOfficial <musengimanajacques@gmail.com>
2026-03-27 15:45:56 +0200

    fixing

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -81,7 +81,7 @@
             <div class="icon">
               <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="{{route('wallet.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{route('admin.wallet.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
