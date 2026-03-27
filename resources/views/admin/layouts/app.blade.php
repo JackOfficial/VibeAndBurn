1db5ce017a084ff2a226438853e2cbdb07a9adf5
JackOfficial <musengimanajacques@gmail.com>
2026-03-27 17:21:40 +0200

    fixing

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -406,7 +406,7 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="{{ route('users.index') }}" class="nav-link">
+                <a href="{{ route('admin.users.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>Manage Users</p>
                 </a>
