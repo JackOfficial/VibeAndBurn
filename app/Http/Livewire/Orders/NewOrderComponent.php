11fb8d2d1279fca39dc4edadab733a22ba91def5
JackOfficial <musengimanajacques@gmail.com>
2026-03-28 14:20:56 +0200

    fixing

--- app/Http/Livewire/Orders/NewOrderComponent.php
+++ app/Http/Livewire/Orders/NewOrderComponent.php
@@ -175,6 +175,7 @@ class NewOrderComponent extends Component
 
             // 5. API Logic
             if ($service->serviceId) {
+              dd(class_exists('App\Http\Controllers\OrderController'));
                 $apiController = new OrderController();
                 
                 /**
