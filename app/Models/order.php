44aab1c2206fef44858d872499c73b63a6f34822
JackOfficial <musengimanajacques@gmail.com>
2026-03-29 10:40:23 +0200

    fixing

--- app/Models/order.php
+++ app/Models/order.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class order extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id', 'service_id', 'link', 'orderId', 'quantity', 'comment', 'charge', 'start_count', 'remains', 'status'];
+    protected $fillable = ['user_id', 'source_id', 'service_id', 'link', 'orderId', 'quantity', 'comment', 'charge', 'start_count', 'remains', 'status'];
 
 public function service()
 {
