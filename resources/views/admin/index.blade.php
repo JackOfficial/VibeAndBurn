97d3f5ae3e943e907278f524b7d5d6a5333b0ea4
JackOfficial <musengimanajacques@gmail.com>
2026-03-29 06:21:35 +0200

    fixing

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -66,7 +66,7 @@
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
-            <a href="{{route('subscription.index')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="{{ route('admin.subscription.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
