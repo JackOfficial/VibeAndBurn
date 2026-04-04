6db998618181cb1b53fbbfb6c4a5655c111901dc
JackOfficial <musengimanajacques@gmail.com>
2026-04-04 05:36:54 +0200

    fixing

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -276,6 +276,7 @@
          <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav mx-auto">
         <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
+        <li class="nav-item"><a class="nav-link active" href="#">About</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('our-services.index') }}">Services</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('faq.index') }}">Help</a></li>
         <li class="nav-item"><a class="nav-link" href="{{ route('contactus.create') }}">Contact</a></li>
