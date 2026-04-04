016a07d9141774e303b4a3e2f2fbc84f83bc976f
JackOfficial <musengimanajacques@gmail.com>
2026-04-04 09:04:33 +0200

    fixing

--- resources/views/admin/subscriptions/index.blade.php
+++ resources/views/admin/subscriptions/index.blade.php
@@ -1,10 +1,6 @@
 @extends('admin.layouts.app')
 @section('content')
 <div class="content-wrapper">
-    <section class="content-header">
-        <h1>Manage Subscribers</h1>
-    </section>
-
     <section class="content">
         <div class="container-fluid">
             {{-- This calls your new Livewire component --}}
