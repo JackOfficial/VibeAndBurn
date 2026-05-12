2a8a32985c05c5c105a9a37d6c6317f6072a9222
JackOfficial <musengimanajacques@gmail.com>
2026-05-12 14:01:19 +0200

    Extending Searching for service

--- app/Http/Livewire/Admin/EditOrder.php
+++ app/Http/Livewire/Admin/EditOrder.php
@@ -28,12 +28,12 @@ class EditOrder extends Component
        'status' => 'required|integer|in:0,1,2,3,4,5'
     ];
 
-    public function mount($orderID)
+    public function mount()
     {
-   
+      dd($orderID);
         $orderData = order::with(['user', 'service.category.socialmedia'])
             ->findOrFail($this->orderID);
-      dd($orderID);
+   
         $this->loadOrderData($orderData);
        
     }
